d552c2df821344d270fcb61b01b18564cc360735
Ayush Verma <ayushv3533e@gmail.com>
2025-08-10 19:48:09 +0530

    Updated server.js to include serve import and corrected code

--- server/server.js
+++ server/server.js
@@ -2,23 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import {inngest, functions} from './inngest/index.js'
+import { inngest, functions } from './inngest/index.js';
+import { serve } from 'inngest/express';
 
 const app = express();
 
+// Connect to DB
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => 
-    res.send("Server is running")
-)
+// Test route
+app.get('/', (req, res) => {
+    res.send("Server is running");
+});
 
-app.use('/api/inngest', serve({ client: inngest, functions }))
+// Inngest route
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) 
-
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
